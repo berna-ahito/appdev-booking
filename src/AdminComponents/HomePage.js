bf955c585445ddf88a443e1fb5199d04e02174bf
berna <bernadethclaire.ahito@cit.edu>
2025-04-23 11:01:20 +0800

    updated homepage <3<3

--- src/AdminComponents/HomePage.js
+++ src/AdminComponents/HomePage.js
@@ -9,31 +9,33 @@ function HomePage() {
       <HeaderAdmin />
       <Sidebar />
 
-      <div className="homepage-content">
-        <div className="admin-info">
-          <h2>Admin123</h2>
-          <p className="admin-role">Admin</p>
-        </div>
-
-        <div className="dashboard-stats">
-          <div className="stat-card maroon">
-            <h3>Active Students</h3>
-            <p>15</p>
+      <div className="homepage-container">
+        <div className="homepage-content">
+          <div className="admin-info">
+            <h2>Admin123</h2>
+            <p className="admin-role">Admin</p>
           </div>
-          <div className="stat-card maroon">
-            <h3>Active Tutor</h3>
-            <p>10</p>
-          </div>
-          <div className="stat-card yellow">
-            <h3>Active Sessions</h3>
-            <p>22</p>
-          </div>
-        </div>
 
-        <hr className="divider" />
+          <div className="dashboard-stats">
+            <div className="stat-card maroon">
+              <h3>Active Students</h3>
+              <p>15</p>
+            </div>
+            <div className="stat-card maroon">
+              <h3>Active Tutor</h3>
+              <p>10</p>
+            </div>
+            <div className="stat-card yellow">
+              <h3>Active Sessions</h3>
+              <p>22</p>
+            </div>
+          </div>
 
-        <div className="announcements-card">
-          <h3>Announcements</h3>
+          <hr className="divider" />
+
+          <div className="announcements-card">
+            <h3>Announcements</h3>
+          </div>
         </div>
       </div>
     </div>
